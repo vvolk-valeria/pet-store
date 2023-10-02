d59046979431e79ac0f7d9f474bda99c475b1079
Volkova Valeria <vvolk.valeria@gmail.com>
2023-10-02 21:57:53 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import App from './App/App'
 import './index.css'
@@ -8,7 +9,11 @@ import store from './store/store'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <React.StrictMode>
-      <App />
+    <BrowserRouter 
+    basename="pet-store"
+    >
+    <App />
+    </BrowserRouter>
     </React.StrictMode>
   </Provider>
 )
